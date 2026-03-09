69683fce60c8727345e9b1050d4177062a3b48be
Tharun-Kumar-A-01 <tharunkumar8906@gmail.com>
2026-03-09 23:00:29 +0530

    updated backend api url

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const API_BASE = "https://register-event.up.railway.app";
+export const API_BASE = "https://register-events.up.railway.app";
 // export const API_BASE = "http://localhost:8000";
